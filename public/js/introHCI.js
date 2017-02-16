8deb2b3d5f5212e9e7233a8bb34eb9cca8227090
Tamara Zubatiy <tzubatiy@ucsd.edu>
2017-02-16 11:15:42 -0800

    changing AJAX

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -17,6 +17,9 @@ function initializePage() {
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
+
+
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
@@ -27,6 +30,19 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get('/project/'+ idNumber, addProject);
+}
+
+function addProject(result){
+	console.log(result);
+	var projectHTML= '<a href="#" class= "thumbnail">' +
+		'<img src='+ result['image'] + '"class= "img">' +
+		'<p>' + result['title']+ '</p>' + 
+		'<p><small>' + result['summary']+ '</small></p></a>';
+
+	$('#'+'project'+result.id+ ' .details').html(result['summary'])
+
 }
 
 /*
